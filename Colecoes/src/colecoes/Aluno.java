18ce7ca78497920d6adc1dbf26f2b3f3185968d5
GilmarGS <gilmar.silva@ccc.ufcg.edu.br>
2019-06-12 10:39:28 -0300

    add atributo cpf em aluno

--- Colecoes/src/colecoes/Aluno.java
+++ Colecoes/src/colecoes/Aluno.java
@@ -4,11 +4,13 @@ public class Aluno {
 	private String nome;
 	private String matricula;
 	private String email;
+	private String cpf;
 	
-	public Aluno(String nome, String matricula, String email) {
+	public Aluno(String nome, String matricula, String email, String cpf) {
 		this.nome = nome;
 		this.matricula = matricula;
 		this.email = email;
+		this.cpf = cpf;
 	}
 
 	public String getNome() {
@@ -22,9 +24,12 @@ public class Aluno {
 	public String getEmail() {
 		return email;
 	}
+	public String getCpf() {
+		return cpf;
+	}
 	
 	@Override
 	public String toString() {
-		return this.matricula + " - " + this.nome + " - " + this.email;
+		return this.matricula + " - " + this.nome + " - " + this.cpf + " - "  + this.email;
 	}
 }
